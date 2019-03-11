f0b345a15bc5d8f10d18012d6267b3aeae38bf47
Roberto Javier Huerta González <a01273164@sldid13-2.local>
2019-03-11 08:30:01 -0600

    added text component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Card from './Card';
+import Text from './Text';
 
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<Card />
+				<Text 
+					text='CUACCC'
+				/>
 			</div>
 		);
 	}
---
+++ src/Text/Text.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Text.css';
+
+class Text extends Component {
+	render() {
+		let classes = `${this.props.color} ${this.props.weight} ${this.props.size || 'body'}`
+		return (
+			<div className={classes}>
+				{this.props.text}
+			</div>
+		);
+	}
+}
+
+export default Text;
