18dc9d1a80ad93322d68be6bf06b1788bdaf7c6a
Roberto Javier Huerta González <a01273164@sldid13-2.local>
2019-03-11 07:55:29 -0600

    WIP Input field

---
+++ src/InputField/InputField.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class InputField extends Component {
+	render() {
+		return (
+			<div >
+			 CUAC
+			</div>
+		);
+	}
+}
+
+export default InputField;
