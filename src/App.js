277aab595d1390890a3c2c17bd698b19a1f1970c
Roberto Javier Huerta González <a01273164@sldid13-2.local>
2019-03-11 08:11:20 -0600

    added input field

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Card from './Card/Card';
 
 class App extends Component {
-  render() {
-    return (
-      <Card />
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Card />
+			</div>
+		);
+	}
 }
 
 export default App;
